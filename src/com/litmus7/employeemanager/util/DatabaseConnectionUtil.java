e9ebf74a4248a2f266363e8cd1c019ed27d3cde1
Rithas K <rithask@icloud.com>
2025-08-17 21:48:28 +0530

    throw sql error instead of handling it

--- src/com/litmus7/employeemanager/util/DatabaseConnectionUtil.java
+++ src/com/litmus7/employeemanager/util/DatabaseConnectionUtil.java
@@ -3,15 +3,11 @@ package com.litmus7.employeemanager.util;
 import com.litmus7.employeemanager.constants.Constants;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DatabaseConnectionUtil {
 
-    public static Connection getConnection() {
-        try {
-            return DriverManager.getConnection(Constants.DB_URL, Constants.DB_USER, Constants.DB_PASSWORD);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            return null;
-        }
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(Constants.DB_URL, Constants.DB_USER, Constants.DB_PASSWORD);
     }
 }
